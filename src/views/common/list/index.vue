a31c5fe7ddcebc371ddf93a29df92f56ef3a44c4
unknown <songchao@zzc.com>
2018-09-07 15:31:23 +0800

    wuliuqiye#000

--- src/views/common/list/index.vue
+++ src/views/common/list/index.vue
@@ -6,6 +6,8 @@
       :queryList="queryList"
       :tableHeader="tableHeader"
       :tableContent="tableContent"
+      @reset="handleReset"
+      @query="list"
       @show-view="componentName=logisticsEnterpriseDetail"
       @show-edit="componentName=logisticsEnterpriseEdit">
       <component :is="componentName"></component>
@@ -21,7 +23,6 @@ import logisticsEnterpriseDetail from '@/views/basicInfo/logisticsEnterprise/det
 import logisticsEnterpriseEdit from '@/views/basicInfo/logisticsEnterprise/edit'
 import queryList from './data/queryList'
 import tableHeader from './data/tableHeader'
-import tableContent from './data/tableContent'
 
 export default {
   name: "home",
@@ -38,22 +39,30 @@ export default {
       pageSize:10,
       queryList: queryList[pathName],
       tableHeader:tableHeader[pathName],
-      tableContent: tableContent[pathName],
+      tableContent: [],
     }
   },
   mounted(){
-    let params = [this.currentPage,this.pageSize];
-    this.queryList && this.queryList.forEach(item => {
-      params.push(item.val)
-    })
-    getList(...params)
-    .then(res => {
-      if(res.state == 0){
-        this.tableContent = res.data.data.rows;
-      }
-    })
+    this.list();
   },
   methods: {
+    list(){
+      let params = [this.currentPage,this.pageSize];
+      this.queryList && this.queryList.forEach(item => {
+        params.push(item.val)
+      })
+      getList(...params)
+      .then(res => {
+        if(res.state == 0){
+          this.tableContent = res.data.data.rows;
+        }
+      })
+    },
+    handleReset(){
+      this.queryList && this.queryList.forEach(item => {
+        item.val = ''
+      })
+    }
   }
 };
 </script>
