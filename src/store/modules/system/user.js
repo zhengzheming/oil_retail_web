00141e571044355078c6329a93f5533274bf6f37
jackie-zheng <zheming.zheng@zygjny.com>
2018-09-07 14:25:33 +0800

    添加用户数据层 #000

--- src/store/modules/system/user.js
+++ src/store/modules/system/user.js
@@ -1,7 +1,25 @@
+import { createSystemUser } from "@/api/system/user";
+
 const user = {
+  state: {
+    systemUserCreate: {
+      form: {},
+      formRef: null
+    }
+  },
   actions: {
-    "system-user-create:save": function() {
-      console.log(`save....system-user-create....`);
+    "system-user-create:save": function({ state }) {
+      const formRef = state.systemUserCreate.formRef;
+      if (!formRef) return;
+      formRef.validate(valid => {
+        if (valid) {
+          createSystemUser();
+        }
+      });
+    },
+    "systemUserCreate:update-form": function({ state }, { form, formRef }) {
+      state.systemUserCreate.form = form;
+      state.systemUserCreate.formRef = formRef;
     }
   }
 };
