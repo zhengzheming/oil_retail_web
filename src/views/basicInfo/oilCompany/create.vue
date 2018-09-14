2dace05dc8773ec38aaf3ad525f52795f44b4ff6
jackie-zheng <zheming.zheng@zygjny.com>
2018-09-14 09:34:32 +0800

    fix bug #000

--- src/views/basicInfo/oilCompany/create.vue
+++ src/views/basicInfo/oilCompany/create.vue
@@ -226,20 +226,7 @@ export default {
         contactPhone: [{ validator: $verify.getValidator("phone") }]
       },
       form: {
-        files: [
-          {
-            name: "food.jpeg",
-            url:
-              "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
-            type: 1
-          },
-          {
-            name: "food2.jpeg",
-            url:
-              "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
-            type: 2
-          }
-        ]
+        files: []
       },
       ui: {
         attachPaperwork: [],
