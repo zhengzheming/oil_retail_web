57c06e23f76bf62d40f4b78b3d708a130fc7b9c2
jackie-zheng <zheming.zheng@zygjny.com>
2018-09-18 17:22:52 +0800

    fix form-control-string 对数字进行转换 #000

--- src/components/FormControlStatic/index.vue
+++ src/components/FormControlStatic/index.vue
@@ -2,7 +2,7 @@
   <div class="form-control--static">
     <span class="form-control--static__title">{{ title }}</span>
     <span class="form-control--static__text">
-      <span v-if="text">{{ text }}</span>
+      <span v-if="textString">{{ textString }}</span>
       <span v-else><slot/></span>
     </span>
   </div>
@@ -19,6 +19,11 @@ export default {
     text: {
       type: [String, Number],
       default: ""
+    }
+  },
+  computed: {
+    textString() {
+      return String(this.text);
     }
   }
 };
