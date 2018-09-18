ee51abc8136d99b6b5dcfe5a97287acccce6d584
jackie-zheng <zheming.zheng@zygjny.com>
2018-09-18 18:21:02 +0800

    增加油品价格模板 #000

--- src/views/priceManage/priceApply/import.vue
+++ src/views/priceManage/priceApply/import.vue
@@ -13,7 +13,7 @@
               :label="labels['priceTemplate']"
             >
               <a
-                :href="`${getFileUrl}?id=25&fileName=油品价格导入模板`"
+                href="/static/油品价格导入模版.xlsx"
                 target="_blank"
                 class="text-link">点击下载模板</a>
             </el-form-item>
