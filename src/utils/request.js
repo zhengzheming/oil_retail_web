f1a44a151f4d6425d9972b1ab255c269e2116bd9
jackie-zheng <zheming.zheng@zygjny.com>
2018-09-20 11:39:11 +0800

    fix bug: 拦截器 退出登录处理 #000

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { Message } from "element-ui";
-import router from "@/router";
+
 // import store from "@/store";
 // import { getToken } from "@/utils/auth";
 
@@ -10,67 +9,5 @@ const service = axios.create({
   timeout: 5000, // request timeout
   headers: { "X-Requested-With": "XMLHttpRequest" }
 });
-
-// request interceptor
-service.interceptors.request.use(
-  config => {
-    // Do something before request is sent
-    // if (store.getters.token) {
-    //   // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-    //   config.headers["X-Token"] = getToken();
-    // }
-    return config;
-  },
-  error => {
-    // Do something with request error
-    console.log(error); // for debug
-    Promise.reject(error);
-  }
-);
-
-// response interceptor
-service.interceptors.response.use(
-  response => {
-    const res = response.data;
-    if (res.state == 100011000) {
-      Message({
-        message: res.data,
-        type: "error",
-        duration: 3 * 1000
-      });
-      return router.push({ name: "login" });
-    }
-    if (res.state !== 0) {
-      let errMessage = res.data;
-      if ($utils.typeIs(errMessage) === "object") {
-        errMessage =
-          '<p style="line-height: 22px;">' +
-          $utils.getStringFromDeep(errMessage).join("<p></p>") +
-          "</p>";
-      }
-      Message({
-        message: errMessage,
-        type: "error",
-        duration: 3 * 1000,
-        dangerouslyUseHTMLString: true
-      });
-      return Promise.reject("error");
-    }
-    return response.data;
-  },
-  error => {
-    console.log("err" + error); // for debug
-    let errMessage = error.message;
-    if (error.response.status == "403") {
-      errMessage = error.response.data.data;
-    }
-    Message({
-      message: errMessage,
-      type: "error",
-      duration: 3 * 1000
-    });
-    return Promise.reject(error);
-  }
-);
 
 export default service;
